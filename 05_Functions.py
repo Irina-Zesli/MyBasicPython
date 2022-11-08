ef3387913010f2c1c7f4c820b08f7df98828774a
Irina-Zesli <zesli@mail.ru>
2022-11-08 19:21:11 +0300

    Добавлена точка входа в программу в 05_Functions.py

--- 05_Functions.py
+++ 05_Functions.py
@@ -6,13 +6,6 @@ def function_list_5(numbers):
         print(number, end = ' ')
     print("")
     
-numbers1 = [2, 4, 78, 3, 110, 98, 45]
-numbers2 = [5, 12, 3, 220, 13, 101, 870, 6]
-
-function_list_5(numbers1)
-function_list_5(numbers2)
-print("")
-
 # Простой поиск и возвращение минимального элемента в списке
 def get_min_simple(numbers):
     m = numbers[0]
@@ -20,15 +13,6 @@ def get_min_simple(numbers):
         if number < m:
             m = number
     return m
-
-numbers1 = [2, 4, 78, 3, 1, 98, 76, 6]
-numbers2 = [12, 43, 22, 13, 101, 87, 6, 45, 5]
-
-my_min = get_min_simple(numbers1)
-print(my_min)
-my_min = get_min_simple(numbers2)
-print(my_min)
-print("")
 
 # Поиск в списке первого элемента, меньшего my_value.
 def get_first_less(numbers, my_value):
@@ -39,10 +23,33 @@ def get_first_less(numbers, my_value):
     else:
         print("Элемент не найден")
 
-numbers1 = [42, 4, 78, 3, 11, 98, 76, 6, 2, 67]
-numbers2 = [12, 13, 22, 13, 101, 87, 16, 45, 5]
+#Точка входа в программу
+def main():
+    numbers1 = [2, 4, 78, 3, 110, 98, 45]
+    numbers2 = [5, 12, 3, 220, 13, 101, 870, 6]
 
-MIN_VALUE = 30
+    function_list_5(numbers1)
+    function_list_5(numbers2)
+    print("")
+    
+    numbers1 = [2, 4, 78, 3, 1, 98, 76, 6]
+    numbers2 = [12, 43, 22, 13, 101, 87, 6, 45, 5]
 
-get_first_less(numbers1, MIN_VALUE)
-get_first_less(numbers2, 1)
+    my_min = get_min_simple(numbers1)
+    print(my_min)
+    my_min = get_min_simple(numbers2)
+    print(my_min)
+    print("")
+    
+    numbers1 = [42, 4, 78, 3, 11, 98, 76, 6, 2, 67]
+    numbers2 = [12, 13, 22, 13, 101, 87, 16, 45, 5]
+
+    MIN_VALUE = 30
+
+    get_first_less(numbers1, MIN_VALUE)
+    get_first_less(numbers2, 1)
+    
+    return 0
+
+if __name__ == '__main__':
+    main()
